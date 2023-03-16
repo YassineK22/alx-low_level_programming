e30f116152df3ee065c850df35cbc77bf4c3573b
Yassine Kraiem <yassinekraiem22@gmail.com>
2023-03-16 13:14:25 -0700

    fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - A programe that printf with methode puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
