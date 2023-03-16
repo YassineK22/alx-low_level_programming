865ee2da7a78724bd5655a68f805f3d469cdc673
Yassine Kraiem <yassinekraiem22@gmail.com>
2023-03-16 13:38:16 -0700

    did last

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A programe that output a message
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout)
+return (1);
+}
