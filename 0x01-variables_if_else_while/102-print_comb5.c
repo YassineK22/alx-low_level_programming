862e83bd301c774982ac2fbc8adefd87482d5ab0
Yassine Kraiem <yassinekraiem22@gmail.com>
2023-03-20 12:35:03 -0700

    did 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main - A programe that print the signe of the number
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int x, y, vir, space;
+
+vir = ',';
+space = ' ';
+for (x = 0; x < 98; x++)
+{
+for (y = x + 1; y < 99; y++)
+{
+putchar('0' + (x/10));
+putchar('0' + (x%10));
+putchar(space);
+putchar('0' + (y/10));
+putchar('0' + (y%10));
+if (x == 98 && y == 99)
+{
+continue;
+}
+putchar(vir);
+putchar(space);
+}
+}
+}
+putchar('\n');
+return (0);
+}
