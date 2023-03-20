ff9484b626c5261eb38c67d4690c11abfc0e38dd
Yassine Kraiem <yassinekraiem22@gmail.com>
2023-03-20 12:40:48 -0700

    space around / and %

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,11 +19,11 @@ for (x = 0; x <= 98; x++)
 {
 for (y = x + 1; y <= 99; y++)
 {
-putchar('0' + (x/10));
-putchar('0' + (x%10));
+putchar('0' + (x / 10));
+putchar('0' + (x % 10));
 putchar(space);
-putchar('0' + (y/10));
-putchar('0' + (y%10));
+putchar('0' + (y / 10));
+putchar('0' + (y % 10));
 if (x == 98 && y == 99)
 {
 continue;
