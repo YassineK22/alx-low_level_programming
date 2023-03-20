eb93537daad9376645c9304210fea40b79c75629
Yassine Kraiem <yassinekraiem22@gmail.com>
2023-03-20 11:16:37 -0700

    did 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main - A programe that print the signe of the number
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+for(int x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+putchar('\n');
+}
+return (0);
+}
