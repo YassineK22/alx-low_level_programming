6e640ae3f8cbc27f7ebb1c7fcd4f9878741d033a
Yassine Kraiem <yassinekraiem22@gmail.com>
2023-03-20 11:40:28 -0700

    did _

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main - A programe that print the signe of the number
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int x;
+
+for (x = '0'; x >= '9'; x--)
+{
+putchar(x);
+}
+for (x = 'a'; x <= 'f'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
