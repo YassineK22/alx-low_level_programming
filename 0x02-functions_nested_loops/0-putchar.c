af4869233a63e1f645fe3b0f679a9fbe7a89d463
Yassine Kraiem <yassinekraiem22@gmail.com>
2023-03-21 12:26:58 -0700

    did 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main - A programe that _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
