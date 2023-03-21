65256f898c3a20bafa73b2797feaafe1c6d50577
Yassine Kraiem <yassinekraiem22@gmail.com>
2023-03-21 13:07:27 -0700

    did 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <main.h>
+#include <"main.h">
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0.
+ */
 
 int main(){
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
